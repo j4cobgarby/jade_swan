9fbc17c2364d4da199209b69138a18e1fb44f1b9
Jacob Garby <j4cobgarby@gmail.com>
2024-10-27 09:35:39 +0100

    example conditional dialogues

--- dialogues/office.js
+++ dialogues/office.js
@@ -182,4 +182,96 @@ var dialogues = {
             }
         ]
     },
+
+    "java_after_rejection": {
+        "conditions": ["black_market_rejected"],
+        "speaker": "java",
+        "text": "He wouldn't talk to you? I could give you the secret code. But I need you to do something for me first.",
+        "go_to_scene": "",
+        "responses": [
+            {
+                "conditions": [],
+                "text": "Uh oh... What do you have in mind?",
+                "go_to_dialogue": "java_asks_for_boots"
+            }
+        ]
+    },
+    "java_asks_for_boots": {
+        "conditions": [],
+        "speaker": "java",
+        "text": "Get me running boots. I don't run so fast, and I'd like to change that.",
+        "responses": [
+            {
+                "conditions": [],
+                "text": "Well, if you insist.",
+                "go_to_dialogue": "boots_quest_accepted"
+            }
+        ]
+    },
+    "boots_quest_accepted": {
+        "conditions": [],
+        "speaker": "thought",
+        "text": "Quite a strange request... But I'm pretty sure I know a good shoe shop.",
+        "go_to_scene": "car",
+        "responses": []
+    },
+
+    "java_has_beans": {
+        "conditions": ["beans"],
+        "speaker": "java",
+        "text": "W-what? No, I don't know anything about those coffee beans!",
+        "go_to_scene": "",
+        "responses": [
+            {
+                "conditions": [],
+                "text": "What about that coffee?",
+                "go_to_dialogue": "java_has_beans_2"
+            }
+        ]
+    },
+    "java_has_beans_2": {
+        "conditions": ["beans"],
+        "speaker": "java",
+        "text": "The coffee I’m drinking? Well… look, I’ll come clean. Ever since the shipment got hijacked, I’ve only been able to find it at the black market. I couldn’t afford a whole bag of the stuff, honest!",
+        "go_to_scene": "",
+        "responses": [
+            {
+                "conditions": [],
+                "text": "Can you tell me who I can talk to about this?",
+                "go_to_dialogue": "java_has_beans_3"
+            }
+        ]
+    },
+    "java_has_beans_3": {
+        "conditions": ["beans"],
+        "speaker": "java",
+        "text": "Look, the guy running the place. He’s a real snake, that Python. But he’ll vouch for me, and he might be able to tell you where he got the stuff from too. I’ll bet that thief of yours took that jaded bird of yours, too...",
+        "go_to_scene": "",
+        "responses": [
+            {
+                "conditions": [],
+                "text": "I hope you're right, that's an important artifact!",
+                "go_to_dialogue": "java_has_beans_4"
+            }
+        ]
+    },
+    "java_has_beans_4": {
+        "conditions": ["beans"],
+        "speaker": "thought",
+        "text": "So, the black market, huh? Suppose I'd ought to check it out, hopefully I can get in...",
+        "go_to_scene": "car",
+        "responses": []
+    },
+
+    "java_already_met": {
+        "conditions": ["met_java"],
+        "speaker": "java",
+        "text": "Hi Detective. Sorry, give the coffee time to kick in. I'm tired from having to tell people that Javascript isn't related to me.",
+        "go_to_scene": "",
+        "responses": [
+        ]
+    },
+    "java_first_time": {
+        "conditions": []
+    }
 }
